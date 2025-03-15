745d190e9b20cc0abcef17fda48b78f21d9e8ee2
peter1024 <u0052017@gmail..com>
2025-03-15 12:17:18 +0800

    feat: 練習 navlink active class 切換

--- src/App.jsx
+++ src/App.jsx
@@ -6,26 +6,52 @@ import Auth from "./views/layout/Auth";
 import ProtectedRoute from "./views/layout/ProtectedRoute";
 
 const App = () => {
-  const getNavStyle = ({isActive})=>{
+  const getNavStyle = ({ isActive }) => {
     return {
-      'color': isActive && 'red'
-    }
-  }
+      color: isActive && "red",
+    };
+  };
 
   return (
     <>
-      {/* <nav>
-        <NavLink to="/" style={getNavStyle}>首頁</NavLink> | 
-        <NavLink to="/auth/login" style={getNavStyle}>登入</NavLink> | 
-        <NavLink to="/auth/sign_up" style={getNavStyle}>註冊</NavLink>
-      </nav> */}
+      <nav>
+        <NavLink
+          to="/"
+          style={getNavStyle}
+          className={({ isActive }) =>
+            ["apple banana", isActive ? "router-link-active" : null].join(" ")
+          }
+        >
+          首頁
+        </NavLink>{" "}
+        |
+        <NavLink
+          to="/auth/login"
+          style={getNavStyle}
+          className={({ isActive }) =>
+            ["apple banana", isActive ? "router-link-active" : null].join(" ")
+          }
+        >
+          登入
+        </NavLink>{" "}
+        |
+        <NavLink
+          to="/auth/sign_up"
+          style={getNavStyle}
+          className={({ isActive }) =>
+            ["apple banana", isActive ? "router-link-active" : null].join(" ")
+          }
+        >
+          註冊
+        </NavLink>
+      </nav>
       <Routes>
         <Route element={<ProtectedRoute />}>
-          <Route path="/" element={ <Todo /> } />
+          <Route path="/" element={<Todo />} />
         </Route>
-        <Route path="/auth" element={ <Auth /> } >
-          <Route path="login" element={ <Login />} />
-          <Route path="sign_up" element={ <SignUp />} />
+        <Route path="/auth" element={<Auth />}>
+          <Route path="login" element={<Login />} />
+          <Route path="sign_up" element={<SignUp />} />
         </Route>
       </Routes>
     </>
